08d1d10c2c4179f0a5ea32b91b9b6e063c61027a
Pete Richards <peter.l.richards@nasa.gov>
2017-09-06 17:00:47 -0700

    fix autoscale padding and custom ranges

--- src/plugins/plot/src/telemetry/PlotController.js
+++ src/plugins/plot/src/telemetry/PlotController.js
@@ -408,7 +408,6 @@ define([
         if (isTick && !this.synchronized()) {
             return;
         }
-        this.config.yAxis.set('displayRange', this.config.yAxis.get('range'));
         this.config.xAxis.set('range', newRange);
         if (!isTick) {
             this.loadMoreData(newRange, true);
